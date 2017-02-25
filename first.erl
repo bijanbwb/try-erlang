f2e149c921490e117af13c1ffa57925ed4ae9516
Bijan <bijanbwb@gmail.com>
2017-02-25 08:33:09 -0500

    Update first.erl with new functions

--- first.erl
+++ first.erl
@@ -1,5 +1,5 @@
 -module(first).
--export([double/1]).
+-export([mult/2,double/1,triple/1,area/3,square/1]).
 
 mult(X,Y) ->
   X*Y.
@@ -7,3 +7,12 @@ mult(X,Y) ->
 double(X) ->
   mult(2,X).
 
+triple(X) ->
+  mult(3,X).
+
+area(A,B,C) ->
+  S = (A+B+C)/2,
+  math:sqrt(S*(S-A)*(S-B)*(S-C)).
+
+square(A) ->
+  mult(A,A).
