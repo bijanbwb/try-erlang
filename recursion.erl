43426e1c13ee631d5c70b04771dd3dd1e382ff9b
Bijan <bijanbwb@gmail.com>
2017-02-25 17:16:36 -0500

    Add fibonacci example

--- recursion.erl
+++ recursion.erl
@@ -1,7 +1,11 @@
 -module(recursion).
--export([factorial/1]).
+-export([factorial/1,fibonacci/1]).
 
 % Takes an integer and returns the factorial result.
 factorial(0) -> 1;
 factorial(X) when X>0 -> X * factorial(X-1).
 
+% Takes an integer and returns the fibonacci result.
+fibonacci(0) -> 0;
+fibonacci(1) -> 1;
+fibonacci(X) when X>0 -> fibonacci(X-2) + fibonacci(X-1).
