ae7025014f3758d413553214390cbcba97d1f357
Bijan <bijanbwb@gmail.com>
2017-02-25 09:09:05 -0500

    Add a sum function to list_tools

--- list_tools.erl
+++ list_tools.erl
@@ -1,8 +1,13 @@
 -module(list_tools).
--export([double_items/1]).
+-export([double_items/1,sum/1]).
 
 % Takes a list of numbers as an argument and then
 % returns a new list with each number doubled.
 double_items(L) ->
   lists:map(fun (X) -> X*2 end, L).
+
+% Takes a list of numbers as an argument and then
+% returns the sum.
+sum(L) ->
+  lists:foldr(fun (X,Y) -> X+Y end, 0, L).
 
