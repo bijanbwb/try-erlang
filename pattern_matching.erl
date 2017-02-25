588ef402b293bb1bd200ae72fc72dd0f6d6ffb85
Bijan <bijanbwb@gmail.com>
2017-02-25 09:31:14 -0500

    Update pattern matching examples

--- pattern_matching.erl
+++ pattern_matching.erl
@@ -1,5 +1,5 @@
 -module(pattern_matching).
--export([is_zero/1]).
+-export([is_zero/1,how_many_equal/3]).
 
 % Use pattern matching to take a number as an argument
 % and return a boolean to indicate whether or not the
@@ -7,3 +7,10 @@
 is_zero(0) -> true;
 is_zero(_) -> false.
 
+% Use pattern matching to determine which number arguments
+% are equal.
+how_many_equal(X,X,X) -> 3;
+how_many_equal(X,X,_) -> 2;
+how_many_equal(_,X,X) -> 2;
+how_many_equal(X,_,X) -> 2;
+how_many_equal(_,_,_) -> 0.
